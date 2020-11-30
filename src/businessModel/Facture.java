8af202720fac1e57cdcedea2c6609882b2c55767
yassine mabrouk <mabroukyassine70@gmail.com>
2020-12-01 00:10:54 +0100

    implement other classes

--- src/businessModel/Facture.java
+++ src/businessModel/Facture.java
@@ -1,5 +1,47 @@
 package businessModel;
 
+import java.util.Date;
+import java.util.List;
+
 public class Facture {
+
+
+    private String numero;
+    private Date date;
+    //many to one
+    private Client client;
+
+    //One to many
+    private List<LigneDeCommande> lignesDeCommande;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<LigneDeCommande> getLignesDeCommande() {
+        return lignesDeCommande;
+    }
+
+    public void setLignesDeCommande(List<LigneDeCommande> lignesDeCommande) {
+        this.lignesDeCommande = lignesDeCommande;
+    }
+
+    public Facture(String numero, Date date, Client client) {
+        this.numero = numero;
+        this.date = date;
+        this.client = client;
+    }
 
 }
--- src/businessModel/LigneDeCommande.java
+++ src/businessModel/LigneDeCommande.java
@@ -1,5 +1,57 @@
 package businessModel;
 
 public class LigneDeCommande {
+    private Long id;
+    private Integer qte;
+    private Double souTotal;
+    private Facture facture;
+    private Produit produit;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQte() {
+        return qte;
+    }
+
+    public void setQte(Integer qte) {
+        this.qte = qte;
+    }
+
+    public Double getSouTotal() {
+        return souTotal;
+    }
+
+    public void setSouTotal(Double souTotal) {
+        this.souTotal = souTotal;
+    }
+
+    public Facture getFacture() {
+        return facture;
+    }
+
+    public void setFacture(Facture facture) {
+        this.facture = facture;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public LigneDeCommande(Long id, Integer qte, Double souTotal, Facture facture, Produit produit) {
+        this.id = id;
+        this.qte = qte;
+        this.souTotal = souTotal;
+        this.facture = facture;
+        this.produit = produit;
+    }
 }

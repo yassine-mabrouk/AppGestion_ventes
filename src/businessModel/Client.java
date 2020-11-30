52cffc0838c05401707d1840a181b9a46dfcbc2a
yassine mabrouk <mabroukyassine70@gmail.com>
2020-12-01 00:06:05 +0100

    implement client class

--- src/businessModel/Client.java
+++ src/businessModel/Client.java
@@ -1,5 +1,40 @@
 package businessModel;
 
-public class Client {
+import java.util.List;
 
+public class Client {
+    private  Long id;
+    private String nom;
+
+    List<Facture> factures;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Facture> getFactures() {
+        return factures;
+    }
+
+    public void setFactures(List<Facture> factures) {
+        this.factures = factures;
+    }
+
+    public Client(Long id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
 }
+
